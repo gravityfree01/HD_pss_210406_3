2578b5dd60f901e97626e99e1458ab1983704e2b
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-04-06 22:10:59 +0900

    문3) 정수값을 입력하세요 문구 추가

--- HD_pss_210406_3/HD_pss_210406_3/Source.cpp
+++ HD_pss_210406_3/HD_pss_210406_3/Source.cpp
@@ -4,7 +4,7 @@
 
 void main() {
     int diameter;
-    printf("지름의값을 입력하세요\n");
+    printf("지름의값을 입력하세요,정수값을 입력하세요 \n");
     float PI = 3.141592;
     scanf_s("%d", &diameter); 
     float radius = diameter / 2;
