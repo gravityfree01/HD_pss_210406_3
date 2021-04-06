a04b957c025a3d885be7077317b28297547596f8
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-04-06 22:00:30 +0900

    재등록

--- HD_pss_210406_3/HD_pss_210406_3/Source.cpp
+++ HD_pss_210406_3/HD_pss_210406_3/Source.cpp
@@ -4,6 +4,7 @@
 
 void main() {
     int diameter;
+    printf("지름의값을 입력하세요");
     float PI = 3.141592;
     scanf_s("%d", &diameter); 
     float radius = diameter / 2;
@@ -11,7 +12,7 @@ void main() {
     float areaOfCircle = radius * radius * PI;
 
 
-    scanf_s("지름의값을 입력하세요=%d", &diameter);
+    
 
     printf("원의 넓이 = %f, 원의 둘레 = %f\n", roundOfCircle, areaOfCircle);
     fgetc(stdin);
